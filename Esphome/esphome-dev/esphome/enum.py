a031913ec2dc48a5376281a860bea367ba13c0df
effelle <effelle@gmail.com>
2026-02-06 14:30:21 -0300

    Configure gitignore and remove Esphome dev folder from tracking

--- Esphome/esphome-dev/esphome/enum.py
+++
@@ -1,8 +0,0 @@
-"""Enum backports from standard lib."""
-
-from __future__ import annotations
-
-from enum import StrEnum as _StrEnum
-
-# Re-export StrEnum from standard library for backwards compatibility
-StrEnum = _StrEnum
